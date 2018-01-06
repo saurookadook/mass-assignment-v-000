98500c7a1e70a7813da0eff05b874785d355c816
Andy Maskiell <maskiella@gmail.com>
2018-01-06 01:43:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
